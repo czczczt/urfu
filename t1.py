342b7ed2b5f1749faa09b65f522d83e5205f1140
cz <t2kcez0@gmail.com>
2025-09-29 17:04:28 +0500

    Update t1.py

--- t1.py
+++ t1.py
@@ -1,3 +1,32 @@
-x=10
-f=3
-print(x+f)
+from string import *
+x = str(input('строка с маленькой буквы:'))
+wh =int(input('1 если шифр, 0 если дешифр:'))
+def f(x, wh):
+    alp = ''
+    rus = 0
+    if any(i in x for i in 'абвгдеёжзийклмнопрстуфхцчшщъыьэюя'):
+        rus = 1
+
+    if rus == 1:
+        alp = 'абвгдеёжзийклмнопрстуфхцчшщъыьэюя'
+    if rus == 0:
+        alp = ascii_lowercase
+
+    if wh == 1:
+        y = ''
+        c = 1
+        for i in x:
+            bb = alp.index(i)
+            bbb = bb + c
+            y += alp[bbb]
+        return y
+    if wh == 0:
+        y = ''
+        c = 1
+        for i in x:
+            bb = alp.index(i)
+            bbb = bb - c
+            y += alp[bbb]
+        return y
+
+print(f(x, wh))
