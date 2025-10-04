10882700eac34d1d60da49bc3f0da0f23a4cd36e
cz <t2kcez0@gmail.com>
2025-10-04 22:38:16 +0500

    Update and rename task6 to task6.py

---
+++ proga/task6.py
@@ -0,0 +1,35 @@
+aa = input(str('введите слово:')) # abc
+lenn = len(aa) # 3
+slovo = ['_'] * lenn # ___
+q = 6
+print(slovo)
+
+while q:
+    bb = input(str('введите БУКВУ:')) # c
+
+    if aa.count(bb) == 0:
+        print(f'буква неправильная, осталось попыток {q-1}/6')
+        q -= 1
+    if aa.count(bb) == 1:
+        qq = aa.index(bb)
+        slovo[qq] = bb
+
+    hh = []
+    kk = -1
+    if aa.count(bb) > 1:
+        for i in aa:
+            kk += 1
+            if i == bb:
+                hh.append(kk)
+        for i in hh:
+            slovo[i] = bb
+    print(slovo)
+
+
+
+    if '_' not in slovo:
+        print('вы победили')
+        break
+
+if q == 0:
+    print('игра окончена')
