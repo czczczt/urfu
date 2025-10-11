9979cad5c2619b3b13300c31ae0b0366e00f191d
czczczt <t2kcez0@gmail.com>
2025-10-12 02:07:56 +0500

    Добавлено обновление README

--- proga dz/dz 2/2 анализатор текста.py
+++ proga dz/dz 2/2 анализатор текста.py
@@ -14,8 +14,4 @@ for i in a:
     if i == ' ':
         probel += 1
 
-print(f'количество глассных:{glas}, количество неглассных:{neglas}, количество пробелов: {probel}, количество слов: {probel+1}, ')
-
-top3 = sorted(set(a), key=lambda c: a.count(c), reverse=True)[:3]
-top3_counts = [(char, a.count(char)) for char in top3]
-print('Топ 3 самых часто встречающихся символов:', top3_counts)
+print(f'количество глассных:{glas}, количество неглассных:{neglas}, количество пробелов: {probel}, количество слов: {probel+1}, ') # не знаю как подсчитать топ символов, поэтому без него =/
