e85ef5552812567ce8f5622a0672f9f46bc78d75
czczczt <t2kcez0@gmail.com>
2025-10-13 02:25:17 +0500

    сделал 4 задание

--- dz/программирование/dz 2/4 банковский счет.py
+++ dz/программирование/dz 2/4 банковский счет.py
@@ -1,8 +1,56 @@
-Напишите программу, симулирующую операции с банковскими счетами
-Каждая операция должна быть реализована в виде отдельной функции Необходимо реализовать следующие операции:
+bb = 0
+sch = {"osnova": 500}
 
-Перевод
-Пополнение счета
-Списание со счета
-Проверка баланса счета
-Создание счета
+
+while 1:
+    aa = str(input('введите операцию(перевод, пополнение, списание, проверка, создание):'))
+
+    if aa == 'перевод' or aa == 'пополнение' or aa == 'списание':
+        print(f'ваш баланс:{sch["osnova"]}')
+        bb = int(input('введите сумму:'))
+
+    if aa not in ['перевод', 'пополнение', 'списание', 'проверка', 'создание']:
+        print('такой команды нет')
+        continue
+
+    def popol(bb, sch):
+        sch["osnova"] += bb
+        return sch
+
+    def spis(bb, sch):
+        sch["osnova"] -= bb
+        return sch
+
+    def prov(sch):
+        return sch
+
+    def sozd(sch):
+        nazv = input('введите название счета:')
+        balans = int(input('введите баланс:'))
+        sch[nazv] = balans
+        return sch
+
+    def perevod(sch, bb):
+        print(sch)
+        f = input('откуда:')
+        t = input('куда:')
+        sch[f] -= bb
+        sch[t] += bb
+        return sch
+
+    if aa == 'пополнение':
+        sch = popol(bb, sch)
+        print(sch["osnova"])
+
+    if aa == 'списание':
+        sch = spis(bb, sch)
+        print(sch["osnova"])
+
+    if aa == 'проверка':
+        print(prov(sch))
+
+    if aa == 'создание':
+        print(sozd(sch))
+
+    if aa == 'перевод':
+        print(perevod(sch, bb))
