d8d9a0ccc5e45b832b4d052e006256177a3b24f9
czczczt <t2kcez0@gmail.com>
2025-10-13 22:54:09 +0500

    dzshka

--- dz/программирование/dz 2/4 банковский счет.py
+++ dz/программирование/dz 2/4 банковский счет.py
@@ -1,50 +1,55 @@
 bb = 0
 sch = {"osnova": 500}
 
+def popol(bb, sch):
+    sch["osnova"] += bb
+    return sch
+
+def spis(bb, sch):
+    sch["osnova"] -= bb
+    return sch
+
+def prov(sch):
+    return sch
+
+def sozd(sch):
+    nazv = input('введите название счета:')
+    balans = int(input('введите баланс:'))
+    sch[nazv] = balans
+    return sch
+
+def perevod(sch, bb):
+    print(sch)
+    f = input('откуда:')
+    t = input('куда:')
+    for i in sch:
+        if f in sch and t in sch:
+            sch[f] -= bb
+            sch[t] += bb
+        else: 
+            print('такого счета нет')
+    return sch
+            
 
 while 1:
     aa = str(input('введите операцию(перевод, пополнение, списание, проверка, создание):'))
 
-    if aa == 'перевод' or aa == 'пополнение' or aa == 'списание':
+    if aa == 'пополнение' or aa == 'списание':
         print(f'ваш баланс:{sch["osnova"]}')
         bb = int(input('введите сумму:'))
 
     if aa not in ['перевод', 'пополнение', 'списание', 'проверка', 'создание']:
         print('такой команды нет')
         continue
-
-    def popol(bb, sch):
-        sch["osnova"] += bb
-        return sch
-
-    def spis(bb, sch):
-        sch["osnova"] -= bb
-        return sch
-
-    def prov(sch):
-        return sch
-
-    def sozd(sch):
-        nazv = input('введите название счета:')
-        balans = int(input('введите баланс:'))
-        sch[nazv] = balans
-        return sch
-
-    def perevod(sch, bb):
-        print(sch)
-        f = input('откуда:')
-        t = input('куда:')
-        sch[f] -= bb
-        sch[t] += bb
-        return sch
+    
 
     if aa == 'пополнение':
         sch = popol(bb, sch)
-        print(sch["osnova"])
+        print(f'ваш баланс: {sch["osnova"]}')
 
-    if aa == 'списание':
+    if aa == 'списание': 
         sch = spis(bb, sch)
-        print(sch["osnova"])
+        print(f'ваш баланс: {sch["osnova"]}')
 
     if aa == 'проверка':
         print(prov(sch))
@@ -54,3 +59,7 @@ while 1:
 
     if aa == 'перевод':
         print(perevod(sch, bb))
+
+    for i in sch:
+        if sch[i] < 0:
+            print('ваш баланс отрицательный, коллекторы уже выехали')
