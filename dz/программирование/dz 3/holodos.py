736dd0888ca5e5b6808b4968eeb251ca5ccf363a
czczczt <t2kcez0@gmail.com>
2025-11-26 21:42:26 +0500

    xz

--- dz/программирование/dz 3/holodos.py
+++ dz/программирование/dz 3/holodos.py
@@ -1,43 +1,102 @@
-# было лень доделывать
+from datetime import date
+from decimal import Decimal
 
-from datetime import *
+DATE_FORMAT = '%Y-%m-%d'
 
-def addd(bb, cc, dd, goods):
-    if bb not in goods:
-        goods[bb] = [{'amount': cc, 'expiration_date': dat}]
-    else:
-        goods[bb].append({'amount': cc, 'expiration_date': dat})
+
+def add(title, amt, exp_dt, goods):
+    if title not in goods:
+        goods[title] = []
+
+    goods[title].append({
+        'amount': amt,
+        'expiration_date': exp_dt
+    })
     return goods
 
 
-goods = {}
-while 1:
-    aal = ('добавить', '')
-    aa = str(input(f'введите операцию{aal}:'))
+def add_by_note(note, goods):
+    parts = note.split()
 
-    if aa not in aal:
+    if len(parts) < 2:
+        print('ошибка: нужны название и количество')
+        return goods
+
+    date_str = parts[-1]
+    is_date = False
+
+    if len(date_str.split('-')) == 3:
+        is_date = True
+
+    if is_date and len(parts) > 2:
+        title = ' '.join(parts[:-2])
+        amt = Decimal(parts[-2])
+        exp_dt = date.fromisoformat(date_str)
+    else:
+        title = ' '.join(parts[:-1])
+        amt = Decimal(parts[-1])
+        exp_dt = None
+
+    return add(title, amt, exp_dt, goods)
+
+
+def amount(query, goods):
+    query_low = query.lower()
+    total = Decimal('0')
+
+    for title in goods:
+        if query_low in title.lower():
+            for batch in goods[title]:
+                total += batch['amount']
+
+    return total
+
+
+def add_handler(goods):
+    t = input('название продукта: ')
+    a = Decimal(input('количество: '))
+    d = input('срок годности (ГГГГ-ММ-ДД, можно пусто): ')
+    if d:
+        e = date.fromisoformat(d)
+    else:
+        e = None
+    add(t, a, e, goods)
+    print('добавлено')
+
+
+def add_note_handler(goods):
+    note = input('название количество дата (опционально): ')
+    add_by_note(note, goods)
+    print('добавлено')
+
+
+def info_handler(goods):
+    print(goods)
+
+
+def amount_handler(goods):
+    q = input('название продукта: ')
+    print(amount(q, goods))
+
+
+goods = {}
+
+while 1:
+    cmds = ('добавить', 'добавить по записке', 'информация', 'количество')
+    cmd = input(f'введите операцию {cmds}: ')
+
+    if cmd not in cmds:
         print('такой команды нет')
         continue
 
-    if aa == 'добавить':
-        bb = str(input('введите название продукта:'))
-        cc = float(input('введите количество:'))
-        dd = str(input('введите срок годности(ГГГГ.ММ.ДД):')) # 2000-12-20
+    if cmd == 'добавить':
+        add_handler(goods)
 
-        try:
-            a = [int(i) for i in dd.split('.')]
-            a1, a2, a3 = a[0], a[1], a[2]
-            dat = date(a1, a2, a3)
-            
-        
-        finally: pass
+    if cmd == 'добавить по записке':
+        add_note_handler(goods)
 
+    if cmd == 'информация':
+        info_handler(goods)
 
-    print(addd(bb, cc, dd, goods))
-
-
-
-
-# https://colab.research.google.com/drive/1KIcNPurYBJIXfs8QGoe2USLwaR5GRAUS?usp=sharing#scrollTo=cwSoZUSFT8Mi
-
-
+    if cmd == 'количество':
+        amount_handler(goods)
